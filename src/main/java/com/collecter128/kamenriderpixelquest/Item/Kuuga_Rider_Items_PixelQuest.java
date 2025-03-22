422c36b017f37fac2979897992fb15bdc22cc1d5
Collecter128 <Levi.little@ymail.com>
2025-03-22 09:35:27 -0400

    Added a few Super Rising forms for Kuuga

--- src/main/java/com/collecter128/kamenriderpixelquest/Item/Kuuga_Rider_Items_PixelQuest.java
+++ src/main/java/com/collecter128/kamenriderpixelquest/Item/Kuuga_Rider_Items_PixelQuest.java
@@ -26,12 +26,45 @@ public class Kuuga_Rider_Items_PixelQuest {
     public static final DeferredItem<Item> KUUGA_SUPER_RISING_GROWING = ITEMS.register("kuuga_super_rising_growing",
             () -> new RiderFormChangeItem(new Item.Properties(),0,"_super_rising_growing","kuuga","arcle_belt_r",
                     new MobEffectInstance(MobEffects.WEAKNESS, 40, 0,true,false),
+                    new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 0,true,false),
                     new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 1,true,false))
                     .has_basic_model()
                     .AddToList(RiderTabs.KUUGA_TAB_ITEM));
 
+    static final DeferredItem<Item> KUUGA_SUPER_RISING_MIGHTY = ITEMS.register("kuuga_super_rising_mighty",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_super_rising_mighty","kuuga","arcle_belt_r",
+                    new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
+                    new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 1,true,false),
+                    new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 0,true,false),
+                    new MobEffectInstance(Effect_core.PUNCH, 40, 3,true,false))
+                    .has_basic_model()
+                    .AddToList(RiderTabs.KUUGA_TAB_ITEM));
+
+    public static final DeferredItem<Item> KUUGA_SUPER_RISING_DRAGON = ITEMS.register("kuuga_super_rising_dragon",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_super_rising_dragon","kuuga","arcle_belt_r",
+                    new MobEffectInstance(MobEffects.DIG_SPEED, 40, 4,true,false),
+                    new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 2,true,false),
+                    new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false),
+                    new MobEffectInstance(MobEffects.JUMP, 40, 1,true,false))
+                    .AddToList(RiderTabs.KUUGA_TAB_ITEM));
+
+    public static final DeferredItem<Item> KUUGA_SUPER_RISING_PEGASUS = ITEMS.register("kuuga_super_rising_pegasus",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_super_rising_pegasus","kuuga","arcle_belt_r",
+                    new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false),
+                    new MobEffectInstance(MobEffects.JUMP, 40, 6,true,false),
+                    new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 2,true,false),
+                    new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false))
+                    .AddToList(RiderTabs.KUUGA_TAB_ITEM));
+
+    public static final DeferredItem<Item> KUUGA_SUPER_RISING_TITAN = ITEMS.register("kuuga_super_rising_titan",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_super_rising_titan","kuuga","arcle_belt_r",
+                    new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 3,true,false),
+                    new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 4,true,false))
+                    .AddToList(RiderTabs.KUUGA_TAB_ITEM));
+
     public static final DeferredItem<Item> KUUGA_AMAZING_GROWING = ITEMS.register("kuuga_amazing_growing",
             () -> new RiderFormChangeItem(new Item.Properties(),0,"_amazing_growing","kuuga","arcle_belt_r",
+                    new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 1,true,false),
                     new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 2,true,false))
                     .has_basic_model()
                     .AddToList(RiderTabs.KUUGA_TAB_ITEM));
