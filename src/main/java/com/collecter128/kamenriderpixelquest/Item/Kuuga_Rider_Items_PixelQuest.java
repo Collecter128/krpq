aaaa05c96830cfee67a50a60cbc50edd74d8df35
Collecter128 <Levi.little@ymail.com>
2025-03-25 15:11:02 -0400

    Added a few Amazing forms for Kuuga

--- src/main/java/com/collecter128/kamenriderpixelquest/Item/Kuuga_Rider_Items_PixelQuest.java
+++ src/main/java/com/collecter128/kamenriderpixelquest/Item/Kuuga_Rider_Items_PixelQuest.java
@@ -69,6 +69,40 @@ public class Kuuga_Rider_Items_PixelQuest {
                     .has_basic_model()
                     .AddToList(RiderTabs.KUUGA_TAB_ITEM));
 
+    public static final DeferredItem<Item> KUUGA_AMAZING_DRAGON = ITEMS.register("kuuga_amazing_dragon",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_amazing_dragon","kuuga","arcle_belt_r",
+                    new MobEffectInstance(MobEffects.DIG_SPEED, 40, 6,true,false),
+                    new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
+                    new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 2,true,false),
+                    new MobEffectInstance(MobEffects.JUMP, 40, 3,true,false))
+                    .AddToList(RiderTabs.KUUGA_TAB_ITEM));
+
+    public static final DeferredItem<Item> KUUGA_AMAZING_PEGASUS = ITEMS.register("kuuga_amazing_pegasus",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_amazing_pegasus","kuuga","arcle_belt_r",
+                    new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false),
+                    new MobEffectInstance(MobEffects.JUMP, 40, 6,true,false),
+                    new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 3,true,false),
+                    new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 2,true,false))
+                    .AddToList(RiderTabs.KUUGA_TAB_ITEM));
+
+    public static final DeferredItem<Item> KUUGA_AMAZING_TITAN = ITEMS.register("kuuga_amazing_titan",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_amazing_titan","kuuga","arcle_belt_r",
+                    new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 3,true,false),
+                    new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 0,true,false),
+                    new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 6,true,false))
+                    .AddToList(RiderTabs.KUUGA_TAB_ITEM));
+
+    public static final DeferredItem<Item> KUUGA_AMAZING_ULTIMATE = ITEMS.register("kuuga_amazing_ultimate",
+            () -> new RiderFormChangeItem(new Item.Properties(),0,"_amazing_ultimate","kuuga","arcle_belt_ru",
+                    new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 3,true,false),
+                    new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 6,true,false),
+                    new MobEffectInstance(MobEffects.JUMP, 40, 6,true,false),
+                    new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 7,true,false),
+                    new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0,true,false),
+                    new MobEffectInstance(Effect_core.PUNCH, 40, 6,true,false),
+                    new MobEffectInstance(MobEffects.DIG_SPEED, 40, 3,true,false))
+                    .AddToList(RiderTabs.KUUGA_TAB_ITEM));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
